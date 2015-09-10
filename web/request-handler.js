00a78b41ecf650398d1fd9679c4809425fade902
Doug Shamoo <doug.shamoo@gmail.com>
2015-09-09 20:14:54 -0700

    Implement basic GET request handling, some file reading

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,36 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
+// var url = require('url');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
+  // Check if incoming request method is GET
+    // Check if it's asking for / or /index.html
+      // fetch index.html from disk and send back as response
+      // - get asset name
+      var asset = routes[req.url];
+      // console.log('asset:', asset);
+      if (asset) {
+        httpHelpers.serveAssets(res, asset, httpHelpers.sendResponse);
+      }
+      else {
+        // check if we have archived copy
+        archive.serveArchives(req, res, httpHelpers.sendResponse);
+
+
+        // httpHelpers.sendResponse(res, 'File Not Found...', 404);
+      }
+
+        // - callback: sending the response, once the data is collected by fs
+
+};
+
+var routes = {
+  '/': 'index.html',
+  '/index.html': 'index.html',
+  '/styles.css': 'styles.css'
+  // '/www.google.com': 'www.google.com'
 };
